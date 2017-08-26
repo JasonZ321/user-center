38746810504568f931e2d74d43553fe822097d1f
jiashengzhou <zhoujiasheng2012@gmail.com>
2017-08-26 18:16:27 +0800

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Created by zhoujiasheng on 17/8/26.
+ */
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorld {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+}
